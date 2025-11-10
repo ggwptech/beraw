4e9019d271f2bf1672b2fcc2d512a1ec339208a3
lutin <lutinlutin76@gmail.com>
2025-11-10 20:56:40 +0500

    Initial Commit

---
+++ RawDogged/RawDoggedApp.swift
@@ -0,0 +1,17 @@
+//
+//  RawDoggedApp.swift
+//  RawDogged
+//
+//  Created by TestApple on 10.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct RawDoggedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RawDoggedTests/RawDoggedTests.swift
@@ -0,0 +1,17 @@
+//
+//  RawDoggedTests.swift
+//  RawDoggedTests
+//
+//  Created by TestApple on 10.11.2025.
+//
+
+import Testing
+@testable import RawDogged
+
+struct RawDoggedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
